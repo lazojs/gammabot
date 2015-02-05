f983a5d35486df274812d2c487d5e604cfb84ec1
Jason <JStrimpel@walmartlabs.com>
2015-02-05 12:46:24 -0800

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
+[![Build Status](https://travis-ci.org/lazojs/gammabot.svg?branch=master)](https://travis-ci.org/lazojs/gammabot)
+
 # gammabot
+
+> It's funny but is it going to get them off their tractors?
+
 Traces Lazo node module dependencies based on package.json meta data.
+
+## Usage
+
+```javascript
+var gammabot = require('gammabot');
+
+// arguments
+// 1. node modules directory to be scanned
+// 3. callback
+
+gammabot('node_modules', function (err, list) {
+    if (err) {
+        throw err;
+    }
+
+    // all lazo node modules
+    console.log(list.modules);
+    // module version conflicts
+    console.log(list.conflicts);
+});
+```
--- index.js
+++ index.js
@@ -1,3 +1,58 @@
-module.exports = function () {
+var dir = require('node-dir');
+var path = require('path');
+var _ = require('lodash');
 
+module.exports = function (modulesDir, callback) {
+    var index = 0;
+    var modules = [];
+
+    dir.readFiles(path.resolve(modulesDir), { match: /package.json$/ }, function (err, content, next) {
+        if (err) {
+            callback(err, null);
+        }
+
+        try {
+            content = JSON.parse(content);
+        } catch (e) {
+            callback(e, null);
+        }
+
+        if (content.lazo) {
+            modules.push({
+                name: content.name,
+                version: content.version,
+                lazo: content.lazo,
+                data: content,
+                index: index
+            });
+        }
+
+        index++;
+        next();
+    }, function (err, files) {
+            if (err) {
+                callback(err, null);
+            }
+            var retVal = {};
+            var groupedBy;
+
+            retVal.modules = modules.map(function (module) {
+                module.path = files[module.index];
+                delete module.index;
+                return module;
+            });
+
+            groupedBy = _.groupBy(retVal.modules, function (module) {
+                return module.name;
+            });
+
+            retVal.conflicts = {};
+            for (var k in groupedBy) {
+                if (groupedBy[k].length > 1) {
+                    retVal.conflicts[k] = groupedBy[k];
+                }
+            }
+
+            callback(null, retVal);
+    });
 };
